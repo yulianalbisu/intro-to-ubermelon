86c1aa180eda3c78b4e16d13318cb97e1fd0d575
Yuliana <ya.aldrich@yahoo.com>
2021-03-07 04:39:12 +0000

    second commit

--- process.py
+++ process.py
@@ -1,14 +1,13 @@
-log_file = open("um-server-01.txt")##this will open our file saved as
-## a um-server and is made in word
+report_file = open("um-server-01.txt")##
 
 
-def sales_reports(log_file): ##the definition of the function is the file
+def sales_reports(report_file): ##the definition of the function is the file
 
-    for line in log_file: ##for the line in the text file
+    for line in report_file: ##for the line in the text file
         line = line.rstrip()## line is line strip is removing space in right
         day = line[0:3]## day is the line from first to third char
         if day == "Mon":## if the day is mon. monday
             print(line)## print the line
 
 
-sales_reports(log_file)## run the sales report of monday
+sales_reports(report_file)## run the sales report of monday
